ca6d3445686f82dd7f641a56aba0281d677b90e6
STRRL <im@strrl.dev>
2024-08-27 20:41:55 -0700

    feat: bump helm chart version
    Signed-off-by: STRRL <im@strrl.dev>

--- node.tf
+++ node.tf
@@ -33,7 +33,7 @@ resource "helm_release" "node" {
   namespace  = var.namespace
   repository = "https://rss3-network.github.io/helm-charts"
   chart      = "node"
-  version    = "0.4.4"
+  version    = "0.4.7"
 
   values = concat([
     yamlencode(local.node_values)
